5487428b05c107de75aaeb0025da880240d94170
Ethan Hanlon <ehanlon19@gmail.com>
2025-10-04 21:15:50 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# efh.moe
+
+**Acknowledgements:**
+
+- [supermariofps/hatsune-miku-windows-linux-cursors](https://github.com/supermariofps/hatsune-miku-windows-linux-cursors) for the Miku cursor
+- [BestTen](https://flopdesign.booth.pm/items/2747965) font via Booth
---
+++ src/app.tsx
@@ -0,0 +1,17 @@
+export function App() {
+  return (
+    <>
+      <div className="panel">
+        <h1>efh.moe</h1>
+
+        <div className="horizontal">
+          <a href="https://github.com/occultslolem">GitHub</a>
+          <span> | </span>
+          <a href="mailto:me@ethan-hanlon.xyz">me@ethan-hanlon.xyz</a>
+        </div>
+
+        <span>(c) {new Date().getFullYear()} Ethan Hanlon</span>
+      </div>
+    </>
+  )
+}
