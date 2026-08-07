03de0f89fd783aec05cdc4d7cc28e066129513d4
Ethan Hanlon <ehanlon19@gmail.com>
2026-08-07 10:10:39 -0700

    Added a work page

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-export function App() {
+export function Home() {
   return (
     <>
       <div className="panel">
@@ -7,10 +7,58 @@ export function App() {
         <div className="horizontal">
           <a href="https://github.com/occultslolem">GitHub</a>
           <span> | </span>
-          <a href="mailto:me@ethan-hanlon.xyz">me@ethan-hanlon.xyz</a>
+          <a href="/work">Work</a>
+          <span> | </span>
+          <a href="mailto:hanlon.ethan@icloud.com">me@ethan-hanlon.xyz</a>
         </div>
 
         <span>(c) {new Date().getFullYear()} Ethan Hanlon</span>
+      </div>
+    </>
+  )
+}
+
+export function Work() {
+  return (
+    <>
+      <div className="work-page">
+        <h1>Work</h1>
+        <p>
+          For the past year I have worked as a data engineer at a 
+          stealth robotics startup, so a lot of my work isn't publicly available.<br /><br />
+
+          However, here's a list of some of the projects I've worked on that are publicly accessible:
+        </p>
+
+        <div className="panel">
+          <ul>
+            <li>
+              <a href="https://mix.ethan-hanlon.xyz" target="_blank" rel="noopener noreferrer">
+                🍸 The Mix
+              </a> - A listing of cocktail recipes that I built both for my own personal use and to create
+              a more recent showcase of my web development skills. It's built to be super fancy and elegant,
+              and has this neat "flavor network" feature that allows you to see how various cocktails are
+              related to each other based on their ingredients.
+            </li>
+            <li>
+              <a href="https://urbanwestsf.com" target="_blank" rel="noopener noreferrer">
+                👷 Urban West SF
+              </a> - A website I improved for a local construction business in San Francisco. It's a simple, 
+              clean website that showcases their products and services, and is built to be responsive and mobile-friendly.
+            </li>
+            <li>
+              <a href="https://openswarm.com" target="_blank" rel="noopener noreferrer">
+                🐞 OpenSwarm
+              </a> - I contributed a Spotify integration to this open-source application.
+            </li>
+            <li>
+              <a href="https://github.com/occultslolem" target="_blank" rel="noopener noreferrer">
+                🧑‍💻 My GitHub
+              </a> - A collection of my open-source projects and contributions. Basically a bunch of gizmos and stuff I've worked on.
+            </li>
+          </ul>
+        </div>
+        <a href="/">&lt;- Home</a>
       </div>
     </>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,19 @@
 import { render } from 'preact'
 import './index.css'
-import { App } from './app.tsx'
+import { ErrorBoundary, LocationProvider, Route, Router } from 'preact-iso'
+import { Home, Work } from './app'
+
+function App() {
+  return (
+    <LocationProvider>
+      <ErrorBoundary>
+        <Router>
+          <Route path="/" component={Home} />
+          <Route path="/work" component={Work} />
+        </Router>
+      </ErrorBoundary>
+    </LocationProvider>
+  )
+}
 
 render(<App />, document.getElementById('app')!)
